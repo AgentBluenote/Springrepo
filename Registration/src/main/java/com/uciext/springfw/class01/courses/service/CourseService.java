25960536b73ab4becafa77412e7ead1823ed9103
Michael <Michael@Michael-PC>
2017-01-22 12:52:03 -0500

    deleted Registration files

--- Registration/src/main/java/com/uciext/springfw/class01/courses/service/CourseService.java
+++
@@ -1,7 +0,0 @@
-package com.uciext.springfw.class01.courses.service;
-
-import com.uciext.springfw.class01.courses.model.Course;
-
-public interface CourseService {
-	public Course getCourse(String courseId);
-}
